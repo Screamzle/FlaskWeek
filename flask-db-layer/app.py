c2c3704de230dff58d869ab2fc4b7f2ff7d40634
M Hutt <matthewehutton@gmail.com>
2022-02-07 17:43:03 +0000

    fixed error where wrong database was being called, changed to testdb from gameshop db - can now add tables and entries

--- flask-db-layer/app.py
+++ flask-db-layer/app.py
@@ -5,10 +5,13 @@ import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
 
-db.create_all()
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+
 print(db)
 
 if __name__ == "__main__":
